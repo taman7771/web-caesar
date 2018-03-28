20a6f78876bef546eae1f622c9b0f5408fb93c50
Samuel Tekle <tsamtek@yahoo.com>
2018-03-27 23:03:13 -0500

    encryption finalized

---
+++ main.py
@@ -0,0 +1,56 @@
+from flask import Flask, request
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form ='''<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+      <form method= 'POST'>
+         <label>Rotate by:
+            <input name="rot" type="text" value = 0>
+         </label>
+         <br><br>
+         <textarea name="text"></textarea>
+         <br>
+         <input type="submit"/>
+</form>
+    </body>
+</html>'''
+
+
+
+
+@app.route("/")
+def index():
+    return form
+
+@app.route("/", methods=['POST'])
+def encrypt():
+    rot_value = int(request.form['rot'])
+    orgn_text = request.form['text']
+    final_rotated = rotate_string(orgn_text, rot_value)
+
+
+    return '<h1> ' + final_rotated + '</h1>'
+
+app.run()
